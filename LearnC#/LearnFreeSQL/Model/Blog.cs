2a4e12cc0eb0f60a089f07bc4b4716985ea90951
mousd <mousd@qq.com>
2023-07-10 20:26:44 +0800

    add learn freesql

---
+++ LearnC#/LearnFreeSQL/Model/Blog.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FreeSql.DataAnnotations;
+using System;
+using System.Diagnostics;
+
+namespace LearnFreeSQL.Model
+{
+    
+
+    public class Blog
+    {
+        [Column(IsIdentity = true, IsPrimary = true)]
+        public int BlogId { get; set; }
+        public string Url { get; set; }
+        public int Rating { get; set; }
+    }
+    public class DB
+    {
+        static Lazy<IFreeSql> sqliteLazy = new Lazy<IFreeSql>(() => new FreeSql.FreeSqlBuilder()
+              .UseMonitorCommand(cmd => Trace.WriteLine($"Sql：{cmd.CommandText}"))//监听SQL语句,Trace在输出选项卡中查看
+              .UseConnectionString(FreeSql.DataType.Sqlite, @"Data Source=freedb.db")
+              .UseAutoSyncStructure(true) //自动同步实体结构到数据库，FreeSql不会扫描程序集，只有CRUD时才会生成表。
+              .Build());
+        public static IFreeSql Sqlite => sqliteLazy.Value;
+    }
+
+}
---
+++ LearnC#/LearnFreeSQL/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Diagnostics;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        IFreeSql fsql = DB.Sqlite;
+    }
+}
+
+public class DB
+{
+    static Lazy<IFreeSql> sqliteLazy = new Lazy<IFreeSql>(() => new FreeSql.FreeSqlBuilder()
+          .UseMonitorCommand(cmd => Trace.WriteLine($"Sql：{cmd.CommandText}"))//监听SQL语句,Trace在输出选项卡中查看
+          .UseConnectionString(FreeSql.DataType.SqlServer, @"Data Source=freedb.db")
+          .UseAutoSyncStructure(true) //自动同步实体结构到数据库，FreeSql不会扫描程序集，只有CRUD时才会生成表。
+          .Build());
+    public static IFreeSql Sqlite => sqliteLazy.Value;
+}
